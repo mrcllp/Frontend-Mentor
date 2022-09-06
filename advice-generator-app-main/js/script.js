88cb94ca48d2ddf7d6382e4697dad1b22bcd6d0c
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-06 16:15:17 +0200

    done

--- advice-generator-app-main/js/script.js
+++ advice-generator-app-main/js/script.js
@@ -1,26 +1,25 @@
 const API_LINK = "https://api.adviceslip.com/advice";
 const adviceNumberSpan = document.querySelector('.advice-generator__advice-number')
-const adviceQuote = document.querySelector('.advice-generator__quote')
+const adviceQuote= document.querySelector('.advice-generator__quote')
 const getBtn = document.querySelector('button.advice-generator__btn')
 
 const fetchNewAdvice = async () => {
     const response = await fetch(API_LINK);
-    const advice = await response.json()
+    const advice = await response.json();
     return advice
 };
 
-const renderAdvice = (adviceObject) => {
-    const {id, advice} = adviceObject;
+const renderAdvice = (adviceObj) => {
+    const {id, advice} = adviceObj;
     adviceNumberSpan.textContent = `ADVICE #${id}`;
     adviceQuote.textContent = advice;
-}
-
-const generateNewAdvice = () => {
-    const data = fetchNewAdvice()
-    const advice = data.slip;
- 
 };
 
+const generateNewAdvice = async () => {
+    const data = await fetchNewAdvice()
+    const advice = data.slip;
+    renderAdvice(advice);
+};
 
 window.addEventListener('DOMContentLoaded', () => {
     getBtn.addEventListener('click', generateNewAdvice)
