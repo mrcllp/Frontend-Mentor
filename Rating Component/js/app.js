e9c7ff48bc743285e8f005ab9446f2de2f59e8e1
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-08 21:32:01 +0200

    js start

---
+++ Rating Component/js/app.js
@@ -0,0 +1,5 @@
+const mainContainer = document.querySelector(".main-dontainer")
+const thanks = document.querySelector(".thanks")
+const submitBtn = document.getElementById("submit")
+const rateBtn = document.getElementById("rate")
+
