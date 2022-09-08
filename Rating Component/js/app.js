946db91f9dfd1bf2b7203bc1949a2a0c26a69634
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-08 21:54:12 +0200

    js done

--- Rating Component/js/app.js
+++ Rating Component/js/app.js
@@ -1,5 +1,26 @@
-const mainContainer = document.querySelector(".main-dontainer")
+const mainContainer = document.querySelector(".main-container")
 const thanks = document.querySelector(".thanks")
 const submitBtn = document.getElementById("submit")
-const rateBtn = document.getElementById("rate")
+const rateBtn = document.getElementById("rate-btn")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
+submitBtn.addEventListener("click", () =>  {
+    thanks.classList.remove("hidden")
+    mainContainer.style.display = "none"
+   
+})
+
+rateBtn.addEventListener("click", () => {
+    mainContainer.style.display = "block"
+    thanks.classList.add("hidden")
+  })
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML
+    })
+})
+
+
 
