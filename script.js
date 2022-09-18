cf220f4115b3c63ae21c6b59e07a45cf2b0bb214
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-19 00:24:23 +0200

    added project to index

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const projects = [
-    'advice-generator-app-main',
-    'NFT Preview Card',
-    'Rating Component',
-    'Single Price Grid Component',
-	'ping-coming-soon-page-master',
-	'fylo-landing-page'
+  "advice-generator-app-main",
+  "NFT Preview Card",
+  "Rating Component",
+  "Single Price Grid Component",
+  "ping-coming-soon-page-master",
+  "fylo-landing-page",
+  "rock-paper-scissors-master",
 ];
 
-
-const list = document.getElementById('list');
+const list = document.getElementById("list");
 
 projects.forEach((project, i) => {
-	const listItem = document.createElement('li');
-	const link = document.createElement('a');
-	link.href = `/${project}/index.html`;
-	link.innerText = `${i + 1}. ${formatProjectName(project)}`;
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = `/${project}/index.html`;
+  link.innerText = `${i + 1}. ${formatProjectName(project)}`;
 
-	const img = document.createElement('img');
-	img.src = `/${project}/design/desktop-design.jpg`;
+  const img = document.createElement("img");
+  img.src = `/${project}/design/desktop-design.jpg`;
 
-	link.prepend(img);
-	listItem.appendChild(link);
-	list.appendChild(listItem);
+  link.prepend(img);
+  listItem.appendChild(link);
+  list.appendChild(listItem);
 });
 
 function formatProjectName(name) {
-	return name
-		.split('-')
-		.map(word => word[0].toUpperCase() + word.slice(1))
-		.join(' ');
+  return name
+    .split("-")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
