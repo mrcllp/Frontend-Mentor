95e761c9011398495f5570519b64389ae062b713
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-18 23:49:39 +0200

    ?????????????????????????

--- rock-paper-scissors-master/js/app.js
+++ rock-paper-scissors-master/js/app.js
@@ -1,18 +1,74 @@
 const choices = ["paper", "rock", "scissors"];
-const buttons = document.querySelectorAll(".btn-icons");
+const buttons = document.querySelectorAll(".pick");
+const scoreEl = document.getElementById("score");
+const main = document.getElementById("main");
+const picks = document.getElementById("picks");
+const reset = document.getElementById("reset");
+const userPick = document.getElementById("player_pick");
+const compPick = document.getElementById("computer_pick");
+const winner = document.getElementById("winner");
 
+let score = 0;
 let userChoice = undefined;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     userChoice = button.getAttribute("data-choice");
 
-    console.log(userChoice);
+    checkWinner();
   });
 });
 
-console.log(pickRandom());
+reset.addEventListener("click", () => {
+  main.style.display = "flex";
+  picks.style.display = "none";
+});
+
+function checkWinner() {
+  const computerChoice = pickRandom();
+
+  updatePick(userPick, userChoice);
+  updatePick(compPick, computerChoice);
+
+  if (userChoice === computerChoice) {
+    //draw
+    winner.inntertext = "draw";
+  } else if (
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    // user won
+    updateScore();
+    winner.innerText = "You Win";
+  } else {
+    // user lost
+    winner.innerText = "You Lose";
+  }
+
+  // show picks and hide main
+  main.style.display = "none";
+  picks.style.display = "flex";
+}
+
+function updateScore() {
+  score += 1;
+
+  scoreEl.innerText = score;
+}
 
 function pickRandom() {
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function updatePick(pickEl, choice) {
+    //reset
+  pickEl.classList.remove("btn-paper");
+  pickEl.classList.remove("btn-rock");
+  pickEl.classList.remove("btn-scissors");
+    // update img
+  const img = pickEl.querySelector("img");
+  pickEl.classList.add(`btn-${choice}`);
+  img.src = `./images/icon-${choice}.svg`;
+  img.alt = choice;
 }
